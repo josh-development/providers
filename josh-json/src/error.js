20e8ce81a05977fe97c08f4271b99db30fe9e50b
DanCodes <dan@dancodes.online>
2021-01-15 23:15:53 +0100

    name of err

--- josh-json/src/error.js
+++ josh-json/src/error.js
@@ -2,7 +2,7 @@ class CustomError extends Error {
   constructor(message, name = null) {
     super();
     Error.captureStackTrace(this, this.constructor);
-    this.name = name || 'JOSHMongoProviderError';
+    this.name = name || 'JOSHJSONProviderError';
     this.message = message;
   }
 }
