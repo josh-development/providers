1fe7fb617c294840cc5fff827332f74402649f2b
RealShadowNova <real.shadow.nova@gmail.com>
2021-07-31 03:49:39 -0400

    chore: remove utilities package

--- packages/utilities/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/get';
-export * from './lib/set';
--- packages/utilities/src/lib/get.ts
+++
@@ -1,5 +0,0 @@
-export function get<Data extends Record<string, any>>(data: Data | undefined, path: string): Data | undefined {
-	const fullPath = path.replace(/\[/g, '.').replace(/]/g, '').split('.').filter(Boolean);
-
-	return fullPath.every((step) => !(step && (data = data?.[step]) === undefined)) ? data : undefined;
-}
