1de1afe1ff624cb6519310412e1f3de297b4ec3e
DanCodes <dan@dancodes.online>
2021-10-28 22:44:42 +0200

    fix(typegoose): warning omission

--- packages/mongo/src/lib/MongoDoc.ts
+++ packages/mongo/src/lib/MongoDoc.ts
@@ -1,5 +1,11 @@
-import { prop } from '@typegoose/typegoose';
+import { modelOptions, prop, Severity } from '@typegoose/typegoose';
 
+@modelOptions({
+	options: {
+		allowMixed: Severity.ALLOW,
+		customName: 'notification'
+	}
+})
 export class MongoDocType {
 	@prop({ required: true })
 	public key!: string;
