58b8cc22a04a292e2b99a281f328bbe1251bb9a8
RealShadowNova <real.shadow.nova@gmail.com>
2022-01-09 15:48:48 -0500

    feat(mongo): add missing re-exports

--- packages/mongo/src/index.ts
+++ packages/mongo/src/index.ts
@@ -1,3 +1,7 @@
+export * from './lib/MongoDoc';
+export * from './lib/MongoDocSchema';
+export * from './lib/MongoDocType';
 export * from './lib/MongoProvider';
+export * from './lib/MongoProviderError';
 
 export const version = '[VI]{version}[/VI]';
