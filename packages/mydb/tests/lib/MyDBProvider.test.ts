731f26bdf6954ad9c3b20de583c7df0ed2a5e45b
RealShadowNova <real.shadow.nova@gmail.com>
2022-05-04 11:40:50 -0400

    chore: oops

--- packages/mydb/tests/lib/MyDBProvider.test.ts
+++
@@ -1,7 +0,0 @@
-import { runProviderTest } from '@joshdb/tests';
-import { MyDBProvider } from '../../src';
-
-runProviderTest<typeof MyDBProvider, MyDBProvider.Options>({
-  providerConstructor: MyDBProvider,
-  providerOptions: {}
-});
