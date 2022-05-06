f6b1d413627fbf9e479bfeb436484d32fcc8d8d1
DanCodes <dan@dancodes.online>
2022-05-06 10:14:36 +0200

    Update packages/redis/tests/lib/RedisProvider.test.ts
    Co-authored-by: Hezekiah Hendry <46537907+RealShadowNova@users.noreply.github.com>

--- packages/redis/tests/lib/RedisProvider.test.ts
+++ packages/redis/tests/lib/RedisProvider.test.ts
@@ -2,6 +2,6 @@ import { runProviderTest } from '@joshdb/tests';
 import { RedisProvider } from '../../src';
 
 runProviderTest<typeof RedisProvider, RedisProvider.Options, RedisProvider>({
-  cleanup: (provider) => provider.close(),
-  providerConstructor: RedisProvider
+  providerConstructor: RedisProvider,
+    cleanup: (provider) => provider.close()
 });
