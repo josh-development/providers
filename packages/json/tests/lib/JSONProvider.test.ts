9b753301c8d75b92bc17178b4bb2da8571efbf92
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-05-29 18:44:12 -0400

    chore:
    permalink:

--- packages/json/tests/lib/JSONProvider.test.ts
+++ packages/json/tests/lib/JSONProvider.test.ts
@@ -1,5 +1,9 @@
 import { runProviderTest } from '@joshdb/provider';
+import { mkdirSync } from 'node:fs';
+import { resolve } from 'node:path';
 import { JSONProvider } from '../../src';
+
+mkdirSync(resolve(process.cwd(), '.tests', 'provider'), { recursive: true });
 
 runProviderTest<typeof JSONProvider, JSONProvider.Options>({
   providerConstructor: JSONProvider,
