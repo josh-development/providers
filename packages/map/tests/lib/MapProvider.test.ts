6c56dad4f9b6817a3209cce5018b6cee7f33e858
DanCodes <dan@dancodes.online>
2022-06-07 20:38:45 +0200

    chore(deps): bump all (#81)
    * chore(deps): bump all
    
    * chore: regen yarn lock

--- packages/map/tests/lib/MapProvider.test.ts
+++ packages/map/tests/lib/MapProvider.test.ts
@@ -2,5 +2,6 @@ import { runProviderTest } from '@joshdb/provider';
 import { MapProvider } from '../../src';
 
 runProviderTest<typeof MapProvider>({
-  providerConstructor: MapProvider
+  providerConstructor: MapProvider,
+  serialization: false
 });
