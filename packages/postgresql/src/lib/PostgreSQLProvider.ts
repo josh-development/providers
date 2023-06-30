88f4b68b92b1504499fce46fc0cfbcc7af5e43da
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2023-06-29 22:58:28 -0400

    fix(PostgreSQLProvider): `Method.Random[Key]` behaviour

--- packages/postgresql/src/lib/PostgreSQLProvider.ts
+++ packages/postgresql/src/lib/PostgreSQLProvider.ts
@@ -509,9 +509,14 @@ export class PostgreSQLProvider<StoredValue = unknown> extends JoshProvider<Stor
     const { count, duplicates } = payload;
     const size = await this.handler.size();
 
-    if (size === 0) return { ...payload, data: [] };
-    if (size < count) {
+    if (!duplicates && size < count) {
       payload.errors.push(this.error({ identifier: CommonIdentifiers.InvalidCount, method: Method.Random }));
+
+      return payload;
+    }
+
+    if (size === 0) {
+      payload.errors.push(this.error({ identifier: CommonIdentifiers.MissingData, method: Method.Random }));
 
       return payload;
     }
