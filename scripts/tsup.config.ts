1d95b74e87879af3ecdf29fcc35e3e3a158fe99e
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2024-04-23 14:49:51 -0400

    feat(deps): update Node to v20 (#381)
    BREAKING CHANGE: Drop support for Node.js v16 and v18

--- scripts/tsup.config.ts
+++ scripts/tsup.config.ts
@@ -11,7 +11,7 @@ export const createTsupConfig = (options: Options = {}) =>
     minify: false,
     skipNodeModulesBundle: true,
     sourcemap: true,
-    target: 'es2021',
+    target: 'es2022',
     tsconfig: relative(__dirname, resolve(process.cwd(), 'src', 'tsconfig.json')),
     keepNames: true,
     esbuildPlugins: [esbuildPluginVersionInjector(), ...(options.esbuildPlugins ?? [])],
